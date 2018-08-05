b92215ce91efc6b138ba1db79874e13695e20b91
sjkelleyjr <sjkelleyjr@gmail.com>
2018-08-04 17:07:26 -0700

    adding eosiolib to directory

---
+++ eosiolib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sjkelleyjr/SoftwareProjects/eos/contracts/eosiolib
+# Build directory: /home/sjkelleyjr/SoftwareProjects/eos/build/contracts/eosiolib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
